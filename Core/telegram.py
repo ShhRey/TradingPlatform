9cbdd91f4e568e0a876093ec76308296038cb795
Shrey Trivedi <41701281+ShhRey@users.noreply.github.com>
2024-01-17 11:08:28 -0500

    Update telegram.py

--- Core/telegram.py
+++ Core/telegram.py
@@ -1,8 +1,8 @@
 import requests
 
 def send(message):
-    apiToken = '6083269588:AAFLellfW9yiDHpc24xTn2lQVzf2F4JjD60'
-    chatID = '-905826135'
+    apiToken = ''
+    chatID = '-'
     apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
     
     while(True):
